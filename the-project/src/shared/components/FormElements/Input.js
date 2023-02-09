a4449ea0d8b65695a010a5297610615c235221d6
ZackSnyder129 <98400006+ZackSnyder129@users.noreply.github.com>
2023-02-09 14:05:31 +0530

    Completed Add Review

--- the-project/src/shared/components/FormElements/Input.js
+++ the-project/src/shared/components/FormElements/Input.js
@@ -1,4 +1,4 @@
-import React,{ useReducer } from 'react';
+import React,{ useReducer, useEffect } from 'react';
 
 import './Input.css';
 import { validate } from '../../util/validators';
@@ -24,6 +24,13 @@ const inputReducer = (state, action) => {
 
 const Input = props => {
     const [inputState, dispatch] = useReducer(inputReducer, {value: '', isTouched: false,isValid: false});
+
+    const{id, onInput} = props;
+    const{value, isValid} = inputState;
+
+    useEffect(() => {
+        onInput(id, value, isValid)
+    }, [id, value, isValid, onInput]);
 
     const changeHandler = event => {
         dispatch({type: 'Change', val: event.target.value, validators: props.validators});
