8371e6de11f75fe8a9d35856e7ed6ab65e50daf3
ZackSnyder129 <98400006+ZackSnyder129@users.noreply.github.com>
2023-02-12 13:50:06 +0530

    Updates

---
+++ precache-manifest.aeb3c4d3a7026d8579da8b9dfe9b06c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a5fedcdeec078690a7a",
+    "url": "/CollegeParkho.github.io/static/css/main.bd183286.chunk.css"
+  },
+  {
+    "revision": "2a5fedcdeec078690a7a",
+    "url": "/CollegeParkho.github.io/static/js/main.2a5fedcd.chunk.js"
+  },
+  {
+    "revision": "d14adb7edfb3e8be709a",
+    "url": "/CollegeParkho.github.io/static/js/1.d14adb7e.chunk.js"
+  },
+  {
+    "revision": "c7c0ca2b15ef4a14af3a",
+    "url": "/CollegeParkho.github.io/static/js/runtime~main.c7c0ca2b.js"
+  },
+  {
+    "revision": "4834ce7686ec25328c2f8af058b372c4",
+    "url": "/CollegeParkho.github.io/index.html"
+  }
+];
